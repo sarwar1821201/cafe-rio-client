c9b39f5eee02fad08c49f34bffc795f3c6e62abe
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-03-11 16:21:52 +0600

    implement header copmpnents

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer";
+import Header from "../pages/Shared/Header";
 
 const MainLayout = () => {
   return (
     <div>
+        <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
