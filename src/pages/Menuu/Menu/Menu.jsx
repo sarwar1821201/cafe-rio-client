05ff211e6442b83707df19976a48febc72210561
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-03-12 03:01:51 +0600

    show today offer on the menu pages

--- src/pages/Menuu/Menu/Menu.jsx
+++ src/pages/Menuu/Menu/Menu.jsx
@@ -3,8 +3,19 @@ import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
 import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+
+    const [menu]= useMenu();
+   const dessert= menu.filter((item) => item.category === "dessert");
+   const soup= menu.filter((item) => item.category === "soup");
+   const salad= menu.filter((item) => item.category === "salad");
+   const pizza= menu.filter((item) => item.category === "pizza");
+   const offered= menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <Helmet>
@@ -12,6 +23,11 @@ const Menu = () => {
       </Helmet>
       <Cover img={menuImg} title={"Our Menu"}></Cover>
      
+      <SectionTitle subHeading="Don't Miss " heading=" Today's Offere "  >
+
+      </SectionTitle>
+
+      <MenuCategory items={offered} ></MenuCategory>
 
     </div>
   );
--- src/pages/Menuu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menuu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import MenuItem from '../../Shared/MenuItem';
 
-const MenuCategory = () => {
+const MenuCategory = ({items}) => {
     return (
         <div>
-            
+            <div className="grid md:grid-cols-2 gap-8">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
         </div>
     );
 };
