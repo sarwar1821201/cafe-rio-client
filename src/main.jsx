9604e8047920cd9d59b510f18441c2b7e306e5a7
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-03-11 16:13:08 +0600

    implement footer

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { router } from './routes/Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-screen-xl mx-auto ' >
     <RouterProvider router={router} ></RouterProvider>
+
+    </div>
   </React.StrictMode>,
 )
