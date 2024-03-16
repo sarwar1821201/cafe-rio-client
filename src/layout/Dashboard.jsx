fc64370c59a27a443dfe45a818b0a2b48b250a43
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-03-16 13:59:32 +0600

    delete cart item

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from 'react-icons/fa';
 import { NavLink, Outlet } from 'react-router-dom';
+import useCart from '../hooks/useCart';
 
 const Dashboard = () => {
+
+   const [cart]= useCart();
+
     return (
         <div className='flex' > 
             <div className="w-64 min-h-screen bg-orange-400 ">
@@ -10,7 +14,7 @@ const Dashboard = () => {
                 <ul className="menu p-4 ">
                 <li>  <NavLink to='/dashboard/userHome' > <FaHome></FaHome>  User Home</NavLink> </li>
                 <li>  <NavLink to='/dashboard/reservation' > <FaCalendar></FaCalendar>  Reservation</NavLink> </li>
-                    <li>  <NavLink to='/dashboard/cart' > <FaShoppingCart></FaShoppingCart>  My Cart</NavLink> </li>
+                    <li>  <NavLink to='/dashboard/cart' > <FaShoppingCart></FaShoppingCart>  My Cart ({cart.length})  </NavLink> </li>
                     <li>  <NavLink to='/dashboard/review' > <FaAd></FaAd> Add Review </NavLink> </li>
                     <li>  <NavLink to='/dashboard/bookings' > <FaList></FaList>  My Bookings</NavLink> </li>
                     <div className='divider' ></div>
