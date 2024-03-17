23e494dfcc7bed8fd6b7ffed70c5a89e0ccbb5da
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-03-18 03:32:01 +0600

    create jwt token and save it localstorage

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
+import useAxiosPublic from '../hooks/useAxiosPublic';
 
 
 
@@ -14,6 +15,7 @@ const AuthProvider = ({children}) => {
     const [loading, setLoading] = useState(true)
  
     const googleProvider= new GoogleAuthProvider();
+    const axiosPublic= useAxiosPublic();
  
     const signUp = (email,password) => {
      setLoading(true)
@@ -45,7 +47,21 @@ const AuthProvider = ({children}) => {
           
      const unsubscribe=  onAuthStateChanged( auth,currentUser => {
              setUser(currentUser)
-             console.log('current user', currentUser)
+            // console.log('current user', currentUser)
+            if(currentUser){
+              const userInfo={email: currentUser.email}
+               axiosPublic.post('/jwt', userInfo)
+               .then( res=> {
+                  if(res.data.token){
+                    localStorage.setItem('access-token', res.data.token )
+                  }
+               } )
+            }
+
+            else{
+               localStorage.removeItem('access-token')
+            }
+             
              setLoading(false)
 
          } )
